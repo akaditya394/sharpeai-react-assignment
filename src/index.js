91a3f2aa9066cdd1c513014e8b91440bdd49b9e8
Aditya <akaditya394@gmail.com>
2023-12-02 19:51:20 +0530

    react router initialized

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Transaction from "./pages/transaction";
+import DataPage from "./pages/data";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Toaster position="top-center" reverseOrder={false} />
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/transaction" element={<Transaction />} />
+      <Route path="/data" element={<DataPage />} />
     </Routes>
   </BrowserRouter>
 );
